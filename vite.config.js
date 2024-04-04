c90eefecd018146f3442f6ee70bdd4113546fa94
kavalcio <sarpkavalcioglu@gmail.com>
2024-04-04 18:35:51 -0400

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'public',
   plugins: [react()],
-})
+  server: {
+    port: 3002,
+  },
+  resolve: {
+    extensions: ['.mdx', '.js', '.jsx'],
+    alias: {
+      '@': '/src',
+    },
+  },
+});
