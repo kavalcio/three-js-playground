a5e44ed4f8d13e7f33dc5809285ca73e5faa3619
kavalcio <sarpkavalcioglu@gmail.com>
2024-04-05 12:34:02 -0400

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Outlet />,
+    errorElement: <div>Error</div>,
+    children: [
+      {
+        path: '/',
+        element: <div>Root</div>,
+      },
+      {
+        path: '/about',
+        element: <div>About</div>,
+      },
+      {
+        path: '/experience',
+        element: <div>Experience</div>,
+      },
+    ],
+  },
+]);
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
