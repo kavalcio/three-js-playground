c1d84d03adac9616c08e4e7ef993d690531ce650
kavalcio <sarpkavalcioglu@gmail.com>
2024-10-14 17:32:20 -0400

    Add drei

--- src/pages/R3FDemo/components/Scene.jsx
+++ src/pages/R3FDemo/components/Scene.jsx
@@ -1,20 +1,19 @@
-import { extend, useFrame, useThree } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
 import * as THREE from 'three';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls, TransformControls } from '@react-three/drei';
 
 import { CustomObject } from '.';
-
-extend({ OrbitControls });
 
 export const Scene = () => {
   const groupRef = useRef();
   const cubeRef = useRef();
+  const sphereRef = useRef();
 
-  const { camera, gl } = useThree();
+  const [rotate, setRotate] = useState(true);
 
   useFrame((state, delta) => {
+    if (!rotate) return;
     cubeRef.current.rotation.x += delta;
     cubeRef.current.rotation.y += delta;
 
@@ -23,17 +22,29 @@ export const Scene = () => {
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls dampingFactor={0.18} makeDefault />
 
       <ambientLight color={0x404040} intensity={1.5} />
       <directionalLight color={0xffffff} intensity={1} position={[3, 3, 3]} />
 
+      <TransformControls
+        object={cubeRef}
+        onMouseDown={() => setRotate(false)}
+        onMouseUp={() => setRotate(true)}
+      />
+
+      <TransformControls
+        object={sphereRef}
+        onMouseDown={() => setRotate(false)}
+        onMouseUp={() => setRotate(true)}
+      />
+
       <group ref={groupRef}>
-        <mesh ref={cubeRef} position={[1, 0, 0]} rotation={[0, Math.PI / 3, 0]}>
+        <mesh ref={cubeRef} position={[2, 0, 0]} rotation={[0, Math.PI / 3, 0]}>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh>
-        <mesh position={[-1, 0, 0]}>
+        <mesh ref={sphereRef} position={[-2, 0, 0]}>
           <sphereGeometry args={[0.7, 32, 32]} />
           <meshStandardMaterial />
         </mesh>
