1fcb209fe2ed7af2ac79e6d8b0ac2cce12a327ec
kavalcio <sarpkavalcioglu@gmail.com>
2024-10-18 12:36:34 -0400

    Add leva ui

--- src/pages/R3FDemo/components/Scene.jsx
+++ src/pages/R3FDemo/components/Scene.jsx
@@ -1,10 +1,13 @@
 import { useFrame } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 import * as THREE from 'three';
-import { OrbitControls, TransformControls } from '@react-three/drei';
+import { OrbitControls, Stats, TransformControls } from '@react-three/drei';
+import { useControls } from 'leva';
 
 import { CustomObject } from '.';
 
+// TODO: add r3f-perf
+// TODO: try tweakpane. it doesn't work with react out of the box, but it's possible to make it work
 export const Scene = () => {
   const groupRef = useRef();
   const cubeRef = useRef();
@@ -20,36 +23,53 @@ export const Scene = () => {
     groupRef.current.rotation.y += delta / 2;
   });
 
+  const { color1, color2, planeCoords, showTransformControls } = useControls({
+    color1: { value: '#ffffff', label: 'Cube color' },
+    color2: { value: '#ff0000', label: 'Sphere color' },
+    planeCoords: { value: { x: 0, z: 0 }, step: 0.1, label: 'Plane coords' },
+    showTransformControls: { value: true, label: 'Show transform controls' },
+  });
+
   return (
     <>
+      <Stats />
+
       <OrbitControls dampingFactor={0.18} makeDefault />
 
       <ambientLight color={0x404040} intensity={1.5} />
       <directionalLight color={0xffffff} intensity={1} position={[3, 3, 3]} />
 
-      <TransformControls
-        object={cubeRef}
-        onMouseDown={() => setRotate(false)}
-        onMouseUp={() => setRotate(true)}
-      />
+      {showTransformControls && (
+        <>
+          <TransformControls
+            object={cubeRef}
+            onMouseDown={() => setRotate(false)}
+            onMouseUp={() => setRotate(true)}
+          />
 
-      <TransformControls
-        object={sphereRef}
-        onMouseDown={() => setRotate(false)}
-        onMouseUp={() => setRotate(true)}
-      />
-
+          <TransformControls
+            object={sphereRef}
+            onMouseDown={() => setRotate(false)}
+            onMouseUp={() => setRotate(true)}
+          />
+        </>
+      )}
       <group ref={groupRef}>
         <mesh ref={cubeRef} position={[2, 0, 0]} rotation={[0, Math.PI / 3, 0]}>
           <boxGeometry />
-          <meshStandardMaterial />
+          <meshStandardMaterial color={color1} />
         </mesh>
         <mesh ref={sphereRef} position={[-2, 0, 0]}>
           <sphereGeometry args={[0.7, 32, 32]} />
-          <meshStandardMaterial />
+          <meshStandardMaterial color={color2} />
         </mesh>
       </group>
-      <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
+      <mesh
+        rotation={[Math.PI / 2, 0, 0]}
+        position-x={planeCoords.x}
+        position-y={-2}
+        position-z={planeCoords.z}
+      >
         <planeGeometry args={[4, 4]} />
         <meshStandardMaterial color={0xbbbbff} side={THREE.DoubleSide} />
       </mesh>
