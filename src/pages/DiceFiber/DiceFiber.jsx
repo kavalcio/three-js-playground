4191ce59738d67fd3ad5cefcbe24d34a323a8bb3
kavalcio <sarpkavalcioglu@gmail.com>
2024-11-28 16:35:11 -0500

    Add perf monitor, add roll result on screen

--- src/pages/DiceFiber/DiceFiber.jsx
+++ src/pages/DiceFiber/DiceFiber.jsx
@@ -1,11 +1,31 @@
 import { Canvas } from '@react-three/fiber';
 
 import { Scene } from './components';
+import { Box, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export const DiceFiber = () => {
+  const [diceRollSum, setDiceRollSum] = useState(null);
   return (
-    <Canvas shadows style={{ height: '100vh', width: '100vw' }}>
-      <Scene />
-    </Canvas>
+    <>
+      <Canvas
+        shadows
+        camera={{ fov: 25, position: [25, 45, 35] }}
+        style={{ height: '100vh', width: '100vw' }}
+      >
+        <Scene diceRollSum={diceRollSum} setDiceRollSum={setDiceRollSum} />
+      </Canvas>
+      {diceRollSum && (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '20%',
+            left: '50%',
+          }}
+        >
+          <Typography>Result: {diceRollSum}</Typography>
+        </Box>
+      )}
+    </>
   );
 };
