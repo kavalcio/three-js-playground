3a360aec36a509799eab34ebb6001e473fdf1caa
Sarp Kavalcioglu <68965649+kavalcio@users.noreply.github.com>
2025-07-18 10:53:24 -0400

    [feat] Solar revamp (#5)
    * Fix rotate issue
    
    * Add textures, add sun object
    
    * Fix uniform rerender issues
    
    * Add atmosphere
    
    * Add specular lighting on earth
    
    * Finish atmosphere

--- src/pages/SolarSystem/components/Scene.jsx
+++ src/pages/SolarSystem/components/Scene.jsx
@@ -1,65 +1,148 @@
 import { OrbitControls, Stats, useTexture } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 import { useControls } from 'leva';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import * as THREE from 'three';
 
+import atmosphereFragmentShader from '../shaders/fragment-atmosphere.glsl';
 import planetFragmentShader from '../shaders/fragment-planet.glsl';
 import planetVertexShader from '../shaders/vertex-planet.glsl';
 
-// TODO: dont forget to normalize light direction
 // TODO: add bloom to sun
+// TODO: add buttons to focus camera on different bodies
+// TODO: add skybox
+// TODO: animate clouds (rotate, also fade parts using perlin noise). maybe even create cloud dynamically
+// TODO: add lens flare to sun
 
-const sunSpherical = new THREE.Spherical(1, Math.PI / 2, Math.PI / 2);
+const SUN_DISTANCE = 5;
+
+const sunDirectionSpherical = new THREE.Spherical(1, Math.PI / 2, Math.PI / 2);
+const sunDirectionCartesian = new THREE.Vector3();
+const sunPosition = new THREE.Vector3();
 
 export const Scene = () => {
-  const sphereRef = useRef();
+  const earthRef = useRef();
+  const sunRef = useRef();
+
+  const { gl: renderer } = useThree();
 
   useFrame((state, delta) => {
-    if (rotate) sphereRef.current.rotation.y += delta / 2;
-    sphereRef.current.material.uniforms.uTime.value += delta;
+    if (rotate) earthRef.current.rotation.y += delta / 4;
+    earthRef.current.material.uniforms.uTime.value += delta;
   });
 
   const textures = useTexture({
     earthDay: '/solar/earth_day.jpg',
     earthNight: '/solar/earth_night.jpg',
-    earthNormal: '/solar/earth_normal.png',
+    // earthNormal: '/solar/earth_normal.png',
     earthSpecular: '/solar/earth_specular.png',
+    earthClouds: '/solar/earth_clouds.jpg',
   });
 
+  const anisotropy = Math.min(renderer.capabilities.getMaxAnisotropy(), 8);
   textures.earthDay.colorSpace = THREE.SRGBColorSpace;
   textures.earthNight.colorSpace = THREE.SRGBColorSpace;
+  textures.earthDay.anisotropy = anisotropy;
+  textures.earthNight.anisotropy = anisotropy;
+  textures.earthClouds.anisotropy = anisotropy;
 
-  const [{ rotate, lightPhi, lightTheta }] = useControls(() => ({
-    // TODO: these phi and theta values stop working when Rotate is turned on
-    rotate: { value: false, label: 'Rotate' },
-    lightPhi: {
-      value: sunSpherical.phi,
-      min: 0,
-      max: Math.PI,
-      label: 'Light Phi',
-      onChange: (value) => {
-        sunSpherical.set(sunSpherical.radius, value, sunSpherical.theta);
-        sphereRef.current.material.uniforms.uLightDirection.value.setFromSpherical(
-          sunSpherical,
-        );
-      },
-    },
-    lightTheta: {
-      value: sunSpherical.theta,
-      min: 0,
-      max: Math.PI * 2,
-      label: 'Light Theta',
-      onChange: (value) => {
-        sunSpherical.set(sunSpherical.radius, sunSpherical.phi, value);
-        sphereRef.current.material.uniforms.uLightDirection.value.setFromSpherical(
-          sunSpherical,
-        );
-      },
-    },
-  }));
+  const updateSunPosition = (value, axis) => {
+    sunDirectionSpherical.set(
+      sunDirectionSpherical.radius,
+      axis === 'phi' ? value : sunDirectionSpherical.phi,
+      axis === 'theta' ? value : sunDirectionSpherical.theta,
+    );
+    earthRef.current.material.uniforms.uLightDirection.value.setFromSpherical(
+      sunDirectionSpherical,
+    );
+    sunPosition
+      .setFromSpherical(sunDirectionSpherical)
+      .multiplyScalar(-SUN_DISTANCE);
+    sunRef.current.position.copy(sunPosition);
+  };
 
-  console.log('rerender', lightPhi, lightTheta);
+  const [{ rotate, atmosphereDayColor, atmosphereTwilightColor }] = useControls(
+    () => ({
+      rotate: { value: false, label: 'Rotate' },
+      cloudDensity: {
+        value: 0.8,
+        min: 0,
+        max: 1,
+        label: 'Cloud Density',
+        onChange: (value) => {
+          earthRef.current.material.uniforms.uCloudDensity.value = value;
+        },
+      },
+      cloudIntensity: {
+        value: 0.5,
+        min: 0,
+        max: 1,
+        label: 'Cloud Intensity',
+        onChange: (value) => {
+          earthRef.current.material.uniforms.uCloudIntensity.value = value;
+        },
+      },
+      lightPhi: {
+        value: sunDirectionSpherical.phi,
+        min: 0,
+        max: Math.PI,
+        label: 'Light Phi',
+        onChange: (value) => updateSunPosition(value, 'phi'),
+      },
+      lightTheta: {
+        value: sunDirectionSpherical.theta,
+        min: 0,
+        max: Math.PI * 2,
+        label: 'Light Theta',
+        onChange: (value) => updateSunPosition(value, 'theta'),
+      },
+      atmosphereDayColor: {
+        value: '#55aaff',
+        label: 'Atmosphere Day Color',
+        onChange: (value) => {
+          earthRef.current.material.uniforms.uAtmosphereDayColor.value.set(
+            value,
+          );
+        },
+      },
+      atmosphereTwilightColor: {
+        value: '#ff4500',
+        label: 'Atmosphere Twilight Color',
+        onChange: (value) => {
+          earthRef.current.material.uniforms.uAtmosphereTwilightColor.value.set(
+            value,
+          );
+        },
+      },
+    }),
+  );
+
+  const uniforms = useMemo(() => {
+    return {
+      uTime: { value: 0 },
+      uLightDirection: { value: sunDirectionCartesian },
+      uDayMap: { value: textures.earthDay },
+      uNightMap: { value: textures.earthNight },
+      uSpecularMap: { value: textures.earthSpecular },
+      uCloudsMap: { value: textures.earthClouds },
+      uCloudDensity: { value: 0.8, type: 'f' },
+      uCloudIntensity: { value: 0.5, type: 'f' },
+      uAtmosphereDayColor: { value: new THREE.Color(atmosphereDayColor) },
+      uAtmosphereTwilightColor: {
+        value: new THREE.Color(atmosphereTwilightColor),
+      },
+      // uNormalMap: { value: textures.earthNormal },
+    };
+  }, [
+    textures.earthDay,
+    textures.earthNight,
+    textures.earthSpecular,
+    textures.earthClouds,
+    atmosphereDayColor,
+    atmosphereTwilightColor,
+  ]);
+
+  console.log('uniforms', uniforms);
 
   return (
     <>
@@ -69,21 +152,27 @@ export const Scene = () => {
 
       <ambientLight color={0x404040} intensity={1.5} />
       <directionalLight color={0xffffff} intensity={1} position={[3, 3, 3]} />
-      <mesh ref={sphereRef} position={[0, 0, 0]}>
+      <mesh ref={earthRef} position={[0, 0, 0]}>
         <sphereGeometry args={[1.5, 64, 64]} />
         <shaderMaterial
           vertexShader={planetVertexShader}
           fragmentShader={planetFragmentShader}
-          uniforms={{
-            uTime: { value: 0 },
-            uLightDirection: {
-              value: new THREE.Vector3().setFromSpherical(sunSpherical),
-            },
-            uDayMap: { value: textures.earthDay },
-            uNightMap: { value: textures.earthNight },
-            uNormalMap: { value: textures.earthNormal },
-          }}
+          uniforms={uniforms}
         />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry args={[1.56, 64, 64]} />
+        <shaderMaterial
+          vertexShader={planetVertexShader}
+          fragmentShader={atmosphereFragmentShader}
+          transparent
+          side={THREE.BackSide}
+          uniforms={uniforms}
+        />
+      </mesh>
+      <mesh ref={sunRef} position={sunPosition}>
+        <sphereGeometry args={[0.4, 16, 16]} />
+        <meshBasicMaterial color={0xffd700} />
       </mesh>
     </>
   );
--- src/pages/SolarSystem/shaders/fragment-planet.glsl
+++ src/pages/SolarSystem/shaders/fragment-planet.glsl
@@ -3,11 +3,20 @@ varying vec3 vModelPosition;
 varying vec3 vNormal;
 
 uniform float uTime;
-// uniform vec3 uColor;
+uniform float uCloudDensity;
+uniform float uCloudIntensity;
+
 uniform vec3 uLightDirection;
+uniform vec3 uAtmosphereDayColor;
+uniform vec3 uAtmosphereTwilightColor;
+
 uniform sampler2D uDayMap;
 uniform sampler2D uNightMap;
-uniform sampler2D uNormalMap;
+uniform sampler2D uCloudsMap;
+uniform sampler2D uSpecularMap;
+// uniform sampler2D uNormalMap;
+
+#include ../../../shaders/lighting/directionalLight.glsl
 
 void main()
 {
@@ -16,14 +25,41 @@ void main()
 
   vec3 color = vec3(0.);
 
-  // Evenly blend between day and night textures based on the UV coordinates
-  float dayFactor = -dot(normal, uLightDirection);
-  dayFactor = smoothstep(-0.25, 0.8, dayFactor);
-  dayFactor = clamp(dayFactor, 0.0, 1.0);
-
+  // Compute day color (incuding clouds)
   vec3 dayColor = texture(uDayMap, vUv).rgb;
+  float cloudFactor = smoothstep(1. - uCloudDensity, 1.0, texture(uCloudsMap, vUv).r) * uCloudIntensity;
+  dayColor = mix(dayColor, vec3(1.0), cloudFactor);
+
+  // Compute night color
   vec3 nightColor = texture(uNightMap, vUv).rgb;
+
+  float sunOrientation = -dot(normal, uLightDirection);
+
+  // Evenly blend between day and night textures based on the UV coordinates
+  float dayFactor = smoothstep(-0.25, 0.8, sunOrientation);
+  dayFactor = clamp(dayFactor, 0.0, 1.0);
   color = mix(nightColor, dayColor, dayFactor);
+
+  // Add a slight atmospheric effect
+  float atmosphereDayFactor = smoothstep(-0.5, 1.0, sunOrientation);
+  atmosphereDayFactor = clamp(atmosphereDayFactor, 0.0, 1.0);
+  vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayFactor);
+  float fresnel = 1. + dot(viewDirection, normal);
+  color = mix(color, atmosphereColor, pow(fresnel, 3.) * atmosphereDayFactor);
+
+  // Add specular light
+  vec3 specularColor = mix(vec3(1.), atmosphereColor, fresnel);
+  float specularIntensity = .6 * texture(uSpecularMap, vUv).r;
+  vec3 light = directionalLight(
+    -uLightDirection,               // Light position
+    viewDirection,                  // View direction
+    normal,                         // Normal
+    specularColor,                  // Light color
+    0.,                             // Diffuse Light intensity
+    specularIntensity,              // Specular Light intensity
+    32.                             // Specular power
+  );
+  color += light;
 
   gl_FragColor = vec4(color, 1.0);
   
